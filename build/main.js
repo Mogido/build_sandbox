3f8107dc16a0694f096cabe9a1620c20467303e8
Maksim <makstrofi@yandex.ru>
2020-05-02 20:59:02 +0300

    learning to use corejs polyfills

--- build/main.js
+++ build/main.js
@@ -1,4 +1,6 @@
-"use strict";
+import "core-js/modules/es.array.flat";
+import "core-js/modules/es.array.unscopables.flat";
+import "core-js/modules/es.promise";
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -6,6 +8,7 @@ class App {
   constructor() {
     _defineProperty(this, "run", async (name = 'World') => {
       console.log(`Hello ${name}`);
+      console.log([1, 2, [2, 3]].flat());
     });
   }
 
