6d889732a329abe0724cec0ca85d9ef439f97dfc
Maksim <makstrofi@yandex.ru>
2020-05-02 21:32:37 +0300

    learning to use react with babel

--- build/main.js
+++ build/main.js
@@ -1,19 +1,6 @@
-import "core-js/modules/es.array.flat";
-import "core-js/modules/es.array.unscopables.flat";
-import "core-js/modules/es.promise";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+const App = () => /*#__PURE__*/React.createElement("p", null, "Hello");
 
-class App {
-  constructor() {
-    _defineProperty(this, "run", async (name = 'World') => {
-      console.log(`Hello ${name}`);
-      console.log([1, 2, [2, 3]].flat());
-    });
-  }
-
-}
-
-;
-const app = new App();
-app.run().then(() => console.log('done')).catch(() => console.log('Error!'));
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App {
+const App = () => <p>Hello</p>;
 
-   run = async (name = 'World') => {
-            console.log(`Hello ${name}`);
-            console.log([1, 2, [2, 3]].flat());
-        };
-    };
-
-const app = new App();
-app.run()
-.then(() => console.log('done'))
-.catch(() => console.log('Error!'));
+ReactDOM.render(<App />,
+     document.getElementById('root'));
